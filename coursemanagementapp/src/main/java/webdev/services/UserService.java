81cf6b347bfd6ba6c624ef14d578d775e4057aee
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-06 19:43:23 -0400

    Added the service class

---
+++ coursemanagementapp/src/main/java/webdev/services/UserService.java
@@ -0,0 +1,38 @@
+package webdev.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import webdev.models.User;
+import webdev.repositories.UserRepository;
+
+@RestController
+public class UserService {
+	@Autowired
+	UserRepository userRepository;
+
+	@GetMapping("/api/user")
+	public Iterable<User> findAllUsers(@RequestParam(name = "username", required = false) String username,
+			@RequestParam(name = "password", required = false) String password) {
+		if (username != null && password != null) {
+			return userRepository.findUserByCredentials(username, password);
+		}
+		return userRepository.findAll();
+	}
+
+	@PostMapping("/api/login")
+	public User login(@RequestBody User user) {
+		List<User> userRecord = (List<User>) userRepository.findUserByCredentials(user.getUsername(),
+				user.getPassword());
+		if (userRecord != null && userRecord.size() > 0) {
+			return user;
+		}
+		return new User();
+	}
+}
