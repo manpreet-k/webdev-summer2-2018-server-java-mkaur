077b9c959bbddc8c083626862fa9ee221e3fb00f
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-06 18:27:14 -0400

    Fixed the fetch parameters

--- coursemanagementapp/src/main/webapp/jquery/services/user.service.client.js
+++ coursemanagementapp/src/main/webapp/jquery/services/user.service.client.js
@@ -2,11 +2,15 @@ function UserService() {
 	this.login = login;
 	this.login_url = '/api/login'
 	var self = this;
-		
+
 	function login(user) {
+		var userObjStr = JSON.stringify(user);
 		return fetch(self.login_url, {
-			method: 'POST',
-			body: JSON.stringify(user)
-		});
+		      method: 'post',
+		      headers: {
+		        'Content-Type': 'application/json'
+		      },
+		      body: userObjStr
+		    });
 	}
 }
