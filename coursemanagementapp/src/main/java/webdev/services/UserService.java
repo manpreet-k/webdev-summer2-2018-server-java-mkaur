825dc7106fed5c433f807537613dd6f7c8c2233c
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-07 00:15:25 -0400

    Service methods for create users and find all users added

--- coursemanagementapp/src/main/java/webdev/services/UserService.java
+++ coursemanagementapp/src/main/java/webdev/services/UserService.java
@@ -35,4 +35,9 @@ public class UserService {
 		}
 		return new User();
 	}
+	
+	@PostMapping("/api/user")
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
 }
