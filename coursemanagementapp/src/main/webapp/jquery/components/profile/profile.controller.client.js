884c5d733901c671fe7616ea55fd21472d8ed494
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-08 17:03:18 -0400

    fetch data to pre-load the fields

--- coursemanagementapp/src/main/webapp/jquery/components/profile/profile.controller.client.js
+++ coursemanagementapp/src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -1,7 +1,7 @@
 (function() {
 	var userService = new UserService();
 	var $success, $error;
-	var $username;
+	var $username, $firstname, $lastname, $email, $phone, $role, $dob;
 	$(main);
 
 	function main() {
@@ -10,21 +10,50 @@
 		$success = $('#successMsg');
 		$error = $('#errorMsg');
 		$username = $('#username');
+		$firstName = $('#firstName');
+		$lastName = $('#lastName');
+		$email = $('#email');
+		$phone = $('#phone');
+		$role = $('#role');
+		$dob = $('#dob');
 
 		$('#logoutBtn').click(logout);
 		$('#updateBtn').click(updateProfile);
 		$username.val(username).prop('disabled', true);
 		$error.hide();
 		$success.hide();
+		findUserByUsername(username);
 	}
+	
+	function findUserByUsername(username) {
+    	userService.findUserByUsername(username, populateData);
+    }
+    
+    function populateData(users) {
+    	var user = users[0];
+    	$firstName.val(user.firstName);
+    	$lastName.val(user.lastName);
+    	$phone.val(user.phone);
+    	$email.val(user.email);	
+    	$dob.val(user.dateOfBirth);	
+		$role.val(user.role);	
+    }
 
-	function logout() {
+	function logout(e) {
+		e.preventDefault();
 		return userService.logout(redirectLogin);
 	}
 
-	function updateProfile() {
-		var user = new User($username.val(), null, null, null, $('#phone').val(),
-				$('#email').val(), $('#role').val(), $('#dob').val())
+	function updateProfile(e) {
+		e.preventDefault();
+		var user = new User($username.val(), 
+							null, 
+							$firstName.val(), 
+							$lastName.val(), 
+							$phone.val(),
+							$email.val(), 
+							$role.val(), 
+							$dob.val())
 		return userService.updateProfile(user, showUpdateMsg);
 	}
 	
